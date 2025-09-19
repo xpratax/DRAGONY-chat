e50322c5004e16af6d147cde39f76e911a5c26d5
xpratax <viniciusdantasg16@gmail.com>
2025-09-19 11:25:36 -0300

    Update chat.js

--- api/chat.js
+++ api/chat.js
@@ -1,11 +1,11 @@
-let messages = []; // Armazena todas as mensagens do chat mundial
+let messages = []; // todas mensagens do chat mundial
 
 export default function handler(req, res){
     if(req.method === 'POST'){
-        const { user, message } = req.body;
-        if(!user || !message) return res.status(400).json({error:'Usuário e mensagem são obrigatórios'});
-        messages.push({user, message, timestamp: new Date()});
-        if(messages.length>100) messages.shift(); // mantém só 100 últimas mensagens
+        const { username, message } = req.body;
+        if(!username || !message) return res.status(400).json({error:'Usuário e mensagem obrigatórios'});
+        messages.push({username, message, timestamp: new Date()});
+        if(messages.length > 100) messages.shift(); // mantém só 100 últimas mensagens
         return res.status(200).json({messages});
     } else if(req.method === 'GET'){
         return res.status(200).json({messages});
@@ -13,3 +13,4 @@ export default function handler(req, res){
         return res.status(405).json({error:'Método não permitido'});
     }
 }
+
